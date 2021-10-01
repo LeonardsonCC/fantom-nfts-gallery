831f61b36dffdfe3237b50b8f3b0737b5349ce7a
LeonardsonCC <leonardson.carvalho@gmail.com>
2021-10-01 20:12:43 -0300

    title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { AppProps } from "next/app";
 import { Web3ReactProvider } from "@web3-react/core";
 import Web3 from "web3";
+import Head from "next/head";
 
 import "../styles/index.css";
 
@@ -12,6 +13,9 @@ function getLibrary(provider) {
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Web3ReactProvider getLibrary={getLibrary}>
+      <Head>
+        <title>Fantom Gallery</title>
+      </Head>
       <Component {...pageProps} />
     </Web3ReactProvider>
   );
